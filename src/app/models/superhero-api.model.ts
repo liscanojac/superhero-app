502185db6fe65456eb2f3cd44adfee787d3280da
Juan Canelon <liscanojac@gmail.com>
2026-05-03 14:46:07 -0300

    feat: superhero api models

---
+++ src/app/models/superhero-api.model.ts
@@ -0,0 +1,70 @@
+
+export type SuperheroPowerstatsApi = {
+  intelligence: string;
+  strength: string;
+  speed: string;
+  durability: string;
+  power: string;
+  combat: string;
+};
+
+export type SuperheroBiographyApi = {
+  'full-name': string;
+  'alter-egos': string;
+  aliases: string[];
+  'place-of-birth': string;
+  'first-appearance': string;
+  publisher: string;
+  alignment: string;
+};
+
+export type SuperheroAppearanceApi = {
+  gender: string;
+  race: string;
+  height: string[];
+  weight: string[];
+  'eye-color': string;
+  'hair-color': string;
+};
+
+export type SuperheroWorkApi = {
+  occupation: string;
+  base: string;
+};
+
+export type SuperheroConnectionsApi = {
+  'group-affiliation': string;
+  relatives: string;
+};
+
+export type SuperheroImageApi = {
+  url: string;
+};
+
+export type SuperheroApiSuccess = {
+  response: 'success';
+  id: string;
+  name: string;
+  powerstats: SuperheroPowerstatsApi;
+  biography: SuperheroBiographyApi;
+  appearance: SuperheroAppearanceApi;
+  work: SuperheroWorkApi;
+  connections: SuperheroConnectionsApi;
+  image: SuperheroImageApi;
+};
+
+export type SuperheroApiError = {
+  response: 'error';
+  error: string;
+};
+
+export type SuperheroApiResponse = SuperheroApiSuccess | SuperheroApiError;
+
+export type SuperheroImageOnlySuccess = {
+  response: 'success';
+  id: string;
+  name: string;
+  url: string;
+};
+
+export type SuperheroImageOnlyResponse = SuperheroImageOnlySuccess | SuperheroApiError;
